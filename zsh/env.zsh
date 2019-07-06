becd274262b7e75e51e41fef520f2189a4c18ec6
TED Vortex <ted.vortex@gmail.com>
2019-07-06 03:33:34 +0300

    feat(zsh): added proper spaceship configuration

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -4,6 +4,22 @@
 export CLICOLOR="YES"
 export LSCOLORS="exgxdHdHcxaHaHhBhDeaec"
 
+# Spaceship settings
+export SPACESHIP_TIME_SHOW=true
+export SPACESHIP_HG_BRANCH_SHOW=false
+export SPACESHIP_HG_STATUS_SHOW=false
+export SPACESHIP_ELM_SHOW=false
+export SPACESHIP_ELIXIR_SHOW=false
+export SPACESHIP_PHP_SHOW=false
+export SPACESHIP_HASKELL_SHOW=false
+export SPACESHIP_VENV_SHOW=false
+export SPACESHIP_CONDA_SHOW=false
+export SPACESHIP_PYENV_SHOW=false
+export SPACESHIP_DOTNET_SHOW=false
+export SPACESHIP_EMBER_SHOW=false
+export SPACESHIP_BATTERY_THRESHOLD=50
+export SPACESHIP_VI_MODE_SHOW=false
+
 # Path
 export PORTSPATH="/opt/local/bin:/opt/local/sbin"
 export PYPATH="/Library/Frameworks/Python.framework/Versions/3.7/bin"
@@ -11,7 +27,6 @@ export CARGOPATH="${HOME}/.cargo/bin"
 export NPMPATH="${HOME}/.npm-global/bin"
 export GOPATH="$(go env GOPATH)/bin"
 
-export PATH="$PATH:$PORTSPATH:$PYPATH:$CARGOPATH:$NPMPATH:$GOPATH"
+export PATH="$NPMPATH:$GOPATH:$CARGOPATH:$PORTSPATH:$PYPATH:$PATH"
 
 typeset -U PATH # Remove duplicates in $PATH
-
