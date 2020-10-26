13d6755ac5e84f83421636becdcd93643d619fa3
TED Vortex <ted.vortex@gmail.com>
2020-10-26 02:21:51 +0200

    feat: grouped symlinks together

---
+++ config/zsh/zshrc.zsh
@@ -0,0 +1,11 @@
+source /etc/profile
+source ~/.zsh_plugins.sh # Load zsh plugins
+
+source ~/.dotfiles/config/zsh/env.zsh
+source ~/.dotfiles/config/zsh/options.zsh
+source ~/.dotfiles/config/zsh/functions.zsh
+source ~/.dotfiles/config/zsh/alias.zsh
+
+source ~/.dotfiles/vendor/git-extras/etc/git-extras-completion.zsh
+
+neofetch -s
--- zsh/zshrc.zsh
+++
@@ -1,9 +0,0 @@
-source /etc/profile
-source ~/.zsh_plugins.sh # Load zsh plugins
-
-source ~/.dotfiles/zsh/env.zsh
-source ~/.dotfiles/zsh/options.zsh
-source ~/.dotfiles/zsh/functions.zsh
-source ~/.dotfiles/zsh/alias.zsh
-
-neofetch -s
