4abcaaff380538c924178c07b0dfe42f6c0c5efe
TED Vortex <ted.vortex@gmail.com>
2021-03-15 06:48:41 +0200

    feat: updated pkgsrc to big sur

--- macos/pkgsrc.sh
+++ macos/pkgsrc.sh
@@ -2,20 +2,19 @@
 
 if [ ! -d /opt/pkg ]; then
   #
-  # Copy and paste the lines below to install the Big Sur set.
+  # Copy and paste the lines below to install the Big Sur on Intel set.
   #
-  # These packages are suitable for anyone running Big Sur x86 (11.0.x) or newer,
-  # and are updated from pkgsrc trunk every few days.
+  # These packages are suitable for anyone running Big Sur (11.0.x) or newer on
+  # Intel CPUs, and are updated from pkgsrc trunk every few days.
   #
-  BOOTSTRAP_TAR="bootstrap-macos11-trunk-x86_64-20201105.tar.gz"
-  BOOTSTRAP_SHA="737ce40a4e30127813bf9e84b085a335e0558ada"
+  BOOTSTRAP_TAR="bootstrap-macos11-trunk-x86_64-20201112.tar.gz"
+  BOOTSTRAP_SHA="b3c0c4286a2770bf5e3caeaf3fb747cb9f1bc93c"
 
   # Download the bootstrap kit to the current directory.
   curl -O https://pkgsrc.joyent.com/packages/Darwin/bootstrap/${BOOTSTRAP_TAR}
 
   # Verify the SHA1 checksum.
-  echo "${BOOTSTRAP_SHA} ${BOOTSTRAP_TAR}" > check-shasum
-  shasum -c check-shasum
+  echo "${BOOTSTRAP_SHA}  ${BOOTSTRAP_TAR}" | shasum -c-
 
   # Verify PGP signature.  This step is optional, and requires gpg.
   curl -O https://pkgsrc.joyent.com/packages/Darwin/bootstrap/${BOOTSTRAP_TAR}.asc
@@ -28,7 +27,6 @@ if [ ! -d /opt/pkg ]; then
   # Reload PATH/MANPATH (pkgsrc installs /etc/paths.d/10-pkgsrc for new sessions)
   eval $(/usr/libexec/path_helper)
 
-  rm check-shasum
   rm ${BOOTSTRAP_TAR}{.asc,}
 else
   echo "pkgsrc already installed"
