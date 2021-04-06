32ff494cf57df06cb6245818e2bfaa3662403997
TED Vortex <ted.vortex@gmail.com>
2021-04-06 03:55:04 +0200

    feat: added proper big sur mac defaults

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -1,88 +1,151 @@
 #!/usr/bin/env sh
 
-# Disable the “Are you sure you want to open this application?” dialog
-defaults write com.apple.LaunchServices LSQuarantine -bool false
+# Global settings
+sudo nvram SystemAudioVolume=%10
+sudo nvram StartupMute=%01
+sudo scutil --set ComputerName "QiD0"
+sudo scutil --set HostName "NARAKU"
+sudo scutil --set LocalHostName "NARAKu"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "NaRAKu"
+sudo systemsetup -settimezone "Africa/Harare" > /dev/null
 
-# Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# General
+defaults write NSGlobalDomain AppleLanguages -array "en" "ro"
+defaults write NSGlobalDomain AppleLocale -string "en_RO"
+defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
+defaults write NSGlobalDomain AppleMetricUnits -bool true
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain AppleInterfaceStyle -string "Dark"
+defaults write NSGlobalDomain AppleAccentColor -int 4
+defaults write NSGlobalDomain AppleHighlightColor -string "0.698039 0.843137 1.000000 Blue"
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+defaults write NSGlobalDomain AppleScrollerPagingBehavior -bool true
 
-# Enable the debug menu in Disk Utility
-defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
-defaults write com.apple.DiskUtility advanced-image-options -bool true
+# Spotlight
+sudo mdutil -a -i off
+#sudo mdutil -i on /Applications
+defaults write com.apple.spotlight orderedItems -array \
+        '{"enabled" = 1;"name" = "APPLICATIONS";}' \
+        '{"enabled" = 1;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}' \
+        '{"enabled" = 1;"name" = "MENU_CONVERSION";}' \
+        '{"enabled" = 1;"name" = "MENU_EXPRESSION";}' \
+        '{"enabled" = 1;"name" = "MENU_DEFINITION";}' \
+        '{"enabled" = 0;"name" = "SYSTEM_PREFS";}' \
+        '{"enabled" = 0;"name" = "DOCUMENTS";}' \
+        '{"enabled" = 0;"name" = "DIRECTORIES";}' \
+        '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
+        '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
+        '{"enabled" = 0;"name" = "PDF";}' \
+        '{"enabled" = 0;"name" = "MESSAGES";}' \
+        '{"enabled" = 0;"name" = "CONTACT";}' \
+        '{"enabled" = 0;"name" = "EVENT_TODO";}' \
+        '{"enabled" = 0;"name" = "IMAGES";}' \
+        '{"enabled" = 0;"name" = "BOOKMARKS";}' \
+        '{"enabled" = 0;"name" = "MUSIC";}' \
+        '{"enabled" = 0;"name" = "MOVIES";}' \
+        '{"enabled" = 0;"name" = "FONTS";}' \
+        '{"enabled" = 0;"name" = "MENU_OTHER";}'
+killall mds > /dev/null 2>&1
 
-# App store
-# Enable the WebKit Developer Tools in the Mac App Store
-# Enable Debug Menu in the Mac App Store
-# Disable the automatic update check
-defaults write com.apple.appstore WebKitDeveloperExtras -bool true
-defaults write com.apple.appstore ShowDebugMenu -bool true
-defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool false
+# Activity Monitor
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+defaults write com.apple.ActivityMonitor IconType -int 6
+defaults write com.apple.ActivityMonitor SelectedTab -int 0
+defaults write com.apple.ActivityMonitor ShowCategory -int 100
+
+# Disk Utility
+defaults write com.apple.DiskUtility "OperationProgress DetailsVisible" -bool true
+defaults write com.apple.DiskUtility SidebarShowAllDevices -bool true
+
+# Safari
+defaults write com.apple.Safari AlwaysRestoreSessionAtLaunch -bool true
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+defaults write com.apple.Safari DownloadsClearingPolicy -int 1
+defaults write com.apple.Safari ExcludePrivateWindowWhenRestoringSessionAtLaunch -bool true
+defaults write com.apple.Safari ExtensionsEnabled -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari NewTabBehavior -int 4
+defaults write com.apple.Safari NewWindowBehavior -int 4
+defaults write com.apple.Safari PasswordBreachDetectionOn -bool false
+defaults write com.apple.Safari PreloadTopHit -bool false
+defaults write com.apple.Safari ShowFavoritesBar-v2 -bool false
+defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
+defaults write com.apple.Safari ShowOverlayStatusBar -bool true
+defaults write com.apple.Safari ShowReadingListInFavorites -bool false
+defaults write com.apple.Safari ShowSidebarInNewWindows -bool false
+defaults write com.apple.Safari ShowSiriSuggestionsPreference -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+
+# Calculator
+defaults write com.apple.calculator ViewDefaultsKey -string "Scientific"
+
+# Control Center
+defaults write com.apple.controlcenter "NSStatusItem Visible Battery" -bool true
+defaults write com.apple.controlcenter "NSStatusItem Visible BentoBox" -bool true
+defaults write com.apple.controlcenter "NSStatusItem Visible Bluetooth" -bool false
+defaults write com.apple.controlcenter "NSStatusItem Visible Clock" -bool true
+defaults write com.apple.controlcenter "NSStatusItem Visible DoNotDisturb" -bool false
+defaults write com.apple.controlcenter "NSStatusItem Visible NowPlaying" -bool false
+defaults write com.apple.controlcenter "NSStatusItem Visible ScreenMirroring" -bool false
+defaults write com.apple.controlcenter "NSStatusItem Visible Sound" -bool true
+defaults write com.apple.controlcenter "NSStatusItem Visible WiFi" -bool true
+
+# Desktop services
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Dock
+defaults write com.apple.dock autohide -bool 0
+defaults write com.apple.dock largesize -float 64
+defaults write com.apple.dock loc -string "en_GB:RO"
+defaults write com.apple.dock orientation -string "left"
+defaults write com.apple.dock region -string "RO"
+defaults write com.apple.dock tilesize -float 32
+defaults write com.apple.dock "trash-full" -bool 1
 
 # Finder
-# Show Hidden Files in Finder
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
-# When performing a search, search the current folder by default
-# Disable the warning when changing a file extension
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 defaults write com.apple.finder ShowPathbar -bool true
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+defaults write com.apple.finder ShowSidebar -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
-defaults write com.apple.finder AppleShowAllFiles YES
+defaults write com.apple.finder SidebarDevicesSectionDisclosedState -bool true
+defaults write com.apple.finder SidebariCloudDriveSectionDisclosedState -bool true
+defaults write com.apple.finder SidebarPlacesSectionDisclosedState -bool true
+defaults write com.apple.finder SidebarShowingiCloudDesktop -bool true
+defaults write com.apple.finder SidebarShowingSignedIntoiCloud -bool true
+defaults write com.apple.finder SidebarWidth -float 150
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 killall Finder
 
-# Activity monitor
-# Show the main window when launching Activity Monitor
-# Visualize CPU usage history in the Activity Monitor Dock icon
-# Show all processes tree view in Activity Monitor
-defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
-defaults write com.apple.ActivityMonitor IconType -int 6
-defaults write com.apple.ActivityMonitor ShowCategory -int 101
+# Trackpad
+defaults write com.apple.AppleMultitouchTrackpad ActuateDetents -int 1
+defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
+defaults write com.apple.AppleMultitouchTrackpad DragLock -int 0
+defaults write com.apple.AppleMultitouchTrackpad Dragging -int 0
+defaults write com.apple.AppleMultitouchTrackpad FirstClickThreshold -int 0
+defaults write com.apple.AppleMultitouchTrackpad ForceSuppressed -bool 0
+defaults write com.apple.AppleMultitouchTrackpad SecondClickThreshold -int 0
+defaults write com.apple.AppleMultitouchTrackpad TrackpadCornerSecondaryClick -int 0
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFiveFingerPinchGesture -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerHorizSwipeGesture -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerPinchGesture -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerVertSwipeGesture -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadHandResting -bool 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadHorizScroll -int 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadMomentumScroll -bool 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadPinch -int 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadRotate -int 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadScroll -bool 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool 0
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerTapGesture -bool 0
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerVertSwipeGesture -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadTwoFingerDoubleTapGesture -bool 1
+defaults write com.apple.AppleMultitouchTrackpad TrackpadTwoFingerFromRightEdgeSwipeGesture -int 3
+defaults write com.apple.AppleMultitouchTrackpad USBMouseStopsTrackpad -int 0
+defaults write com.apple.AppleMultitouchTrackpad UserPreferences -bool 1
+defaults write com.apple.AppleMultitouchTrackpad version -int 12
 
-
-# Reveal IP address, hostname, OS version, etc. when clicking the clock
-# in the login window
-sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
-
-###############################################################################
-# Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
-###############################################################################
-
-# Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
-# Trackpad: sensible defaults
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -int 1
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.fiveFingerPinchSwipeGesture -int 2
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.fourFingerHorizSwipeGesture -int 2
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.fourFingerPinchSwipeGesture -int 2
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.fourFingerVertSwipeGesture -int 2
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.momentumScroll -int 1
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.pinchGesture -int 1
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.rotateGesture -int 1
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.scrollBehavior -int 2
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerDragGesture -int 0
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerHorizSwipeGesture -int 2
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerTapGesture -int 0
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.threeFingerVertSwipeGesture -int 2
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.twoFingerDoubleTapGesture -int 1
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.twoFingerFromRightEdgeSwipeGesture -int 3
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.rotateGesture -int 1
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.rotateGesture -int 1
-
-# Show language menu in the top right corner of the boot screen
-sudo defaults write /Library/Preferences/com.apple.loginwindow showInputMenu -bool true
-
-# Set the timezone; see `sudo systemsetup -listtimezones` for other values
-sudo systemsetup -settimezone "Europe/Bucharest" > /dev/null
-
+# System Preferences
+defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
